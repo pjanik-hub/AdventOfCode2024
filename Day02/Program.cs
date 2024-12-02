f2783a35511531ca34a2dcfbea517ec565d447e4
pjanik-hub <philipjanik3@gmail.com>
2024-12-02 08:14:53 -0700

    init day2

---
+++ Day02/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+
+
+        }
+
+        static void Part1(List<List<int>> reactorValues)
+        {
+
+        }
+    }
+}
