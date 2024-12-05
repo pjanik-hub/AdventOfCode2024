57edd74b3661adc1ff10a569ca96ffd9071db2b6
pjanik-hub <philipjanik3@gmail.com>
2024-12-04 19:28:58 -0700

    day 04 init/start

---
+++ Day04/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Day04
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region read input
+			string[] lines = File.ReadAllLines("input.txt");
+			int rows = lines.Length;
+			int cols = lines[0].Length;
+			char[,] grid = new char[rows, cols];
+
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < cols; j++)
+				{
+					grid[i, j] = lines[i][j];
+				}
+			}
+			#endregion
+
+			string substring = "XMAS";
+			int count = 0;
+
+			Console.WriteLine($"Answer: {count}");
+		}
+	}
+}
