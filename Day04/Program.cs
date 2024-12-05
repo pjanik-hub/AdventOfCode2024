a7731237333e4086ac0357c0dfec3004b47a2255
pjanik-hub <philipjanik3@gmail.com>
2024-12-04 19:45:29 -0700

    closer to part 2 done

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -5,6 +5,7 @@
 		static void Main(string[] args)
 		{
 			#region read input
+			// part 1
 			string[] lines = File.ReadAllLines("input.txt");
 			int rows = lines.Length;
 			int cols = lines[0].Length;
@@ -17,15 +18,75 @@
 					grid[i, j] = lines[i][j];
 				}
 			}
+
+			// part 2
+			lines = File.ReadAllLines("input1.txt");
+			int rows2 = lines.Length;
+			int cols2 = lines[0].Length;
+			char[,] grid2 = new char[rows2, cols2];
+
+			for (int i = 0; i < rows2; i++)
+			{
+				for (int j = 0; j < cols2; j++)
+				{
+					grid2[i, j] = lines[i][j];
+				}
+			}
 			#endregion
 
 			string substring = "XMAS";
 			int count = CountSubstringHVD(grid, substring, rows, cols);
 
+			int xCount = CountXShape(grid2, rows2, cols2);
+
 			Console.WriteLine($"Answer: {count}");
+			Console.WriteLine($"Answer2: {xCount}");
 			Console.ReadKey();
 		}
-		
+
+		static int CountXShape(char[,] grid, int rows, int cols)
+		{
+			int total = 0;
+			string substring = "MAS";
+			int sLen = substring.Length;
+
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < cols; j++)
+				{
+					// check X shape forward
+					if (i <= rows - sLen && j >= 1 && j <= cols - 2 &&
+						grid[i, j] == substring[0] &&
+						grid[i + 1, j - 1] == substring[1] &&
+						grid[i + 2, j] == substring[2] &&
+						grid[i + 1, j + 1] == substring[1])
+					{
+						total++;
+					}
+
+					// check X shape backward
+					if (i <= rows - sLen && j >= 1 && j <= cols - 2 &&
+						grid[i, j] == substring[2] &&
+						grid[i + 1, j - 1] == substring[1] &&
+						grid[i + 2, j] == substring[0] &&
+						grid[i + 1, j + 1] == substring[1])
+					{
+						total++;
+					}
+				}
+			}
+
+			return total;
+		}
+
+		/// <summary>
+		/// H: Horizontal, V: Vertical, D: Diagonal
+		/// </summary>
+		/// <param name="grid"></param>
+		/// <param name="substring"></param>
+		/// <param name="rows"></param>
+		/// <param name="cols"></param>
+		/// <returns>Total occuring substrings</returns>
 		static int CountSubstringHVD(char[,] grid, string substring, int rows, int cols)
 		{
 			int total = 0;
