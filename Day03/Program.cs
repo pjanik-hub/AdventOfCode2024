6365cb4a4489ceffdfca7fa8a3f53c9998e38dd1
pjanik-hub <philipjanik3@gmail.com>
2024-12-04 19:15:44 -0700

    part 1

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,23 +1,34 @@
-﻿namespace Day03
+﻿using System.Text.RegularExpressions;
+
+namespace Day03
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			#region capture input
 			// assume file is good
 			string[] lines = File.ReadAllLines("input.txt");
+
+			int totalSum = 0;
+			// regex matching
+			Regex regex = new Regex(@"mul\((\d+),(\d+)\)");
 
 			// iterate over each row...
 			foreach (string line in lines)
 			{
+				MatchCollection collection = regex.Matches(line);
 
-			}
-			#endregion
+                foreach (Match match in collection)
+                {
+					int a = int.Parse(match.Groups[1].Value);
+					int b = int.Parse(match.Groups[2].Value);
 
+					totalSum += a * b;
+				}
+            }
 
-
-			Console.WriteLine("Hello, World!");
+			Console.WriteLine($"Answer: {totalSum}");
+			Console.ReadKey();
 		}
 	}
 }
