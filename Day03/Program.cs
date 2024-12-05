f24c05f458085347d23c8ec65df56163e6a99b85
pjanik-hub <philipjanik3@gmail.com>
2024-12-04 19:10:46 -0700

    day3 init

---
+++ Day03/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Day03
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region capture input
+			// assume file is good
+			string[] lines = File.ReadAllLines("input.txt");
+
+			// iterate over each row...
+			foreach (string line in lines)
+			{
+
+			}
+			#endregion
+
+
+
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
